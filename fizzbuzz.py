1a0eda9a7d7402224d90bc968c976da93857baef
Kayla Van Bortel <kjv7359@rit.edu>
2024-01-18 09:54:04 -0500

    add fizzbuzz

---
+++ fizzbuzz.py
@@ -0,0 +1,15 @@
+def main():
+    for i in range(1, 101):
+        if i % 5 == 0 and i % 7 == 0:
+            print("FizzBuzz")
+        elif i % 5 == 0:
+            print("Fizz")
+        elif i % 7 == 0:
+            print("Buzz")
+        else:
+            print(i)
+
+
+if __name__ == "__main__":
+    main()
+
