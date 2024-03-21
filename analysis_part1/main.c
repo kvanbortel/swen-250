27067282a0ea326d8013cedb0cfea4b2a52e7ea9
Kayla Van Bortel <kjv7359@rit.edu>
2024-03-21 09:28:01 -0400

    add proj part 1

---
+++ analysis_part1/main.c
@@ -0,0 +1,17 @@
+// main program for the Document Analysis Project
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "unit_tests.h"
+
+
+// Run the unit tests or as a "normal program".
+// You can run this as a "normal program" if you want for a simple test of the sort_two_positions function.
+int main( int argc, char *argv[] )
+{
+	
+	test() ;
+	
+	return 0 ;
+}
