f28aa9409030261e6fc951101f9c883ef215c378
Kayla Van Bortel <kjv7359@rit.edu>
2024-04-25 15:57:46 -0400

    complete tdd_linear

--- tdd_linear/tdd_linear.c
+++ tdd_linear/tdd_linear.c
@@ -40,7 +40,7 @@ int assert( int test_result, char error_format[], ... ) {
 // integer values as the x array based on the passed count of entries in
 // the x array.
 //
-// [done] Returns NULL if the x pointer is NULL.
+// Returns NULL if the x pointer is NULL.
 // Returns NULL if the count parameter is <= 0.
 // The m and b integer parameters can be any valid integer value.
 //
@@ -49,15 +49,19 @@ int assert( int test_result, char error_format[], ... ) {
 //    of the int *x that is passed to this function.
 int *linear( int *x, int count, int m, int b )
 {
-	// int bogus = 0;
-	// int *y = &bogus;	// code to test failure case for NULL input parameter
+	if (!x || count <= 0)
+		return NULL;
 	
-	int *y = NULL;
+	int *y = (int *)malloc(sizeof(int) * count);
+	int *x_work = x;
+	int *y_work = y;
+	int i;
+	for (i = 0; i < count; i++, y_work++, x_work++)
+	{
+		*y_work = *x_work * m + b;
+	}
 
-	if (x)
-		y = (int *)malloc(sizeof(int) * count);
-
-	return y ;
+	return y;
 }
 
 // create unit tests in the correct order by checking for valid
@@ -77,25 +81,68 @@ int unit_tests()
 {
 	int passcount = 0;	// default to failure (returns number of tests that passed)
 	int failcount = 0;
-	int x1[] = {1, 2};
+	int x1[] = {1};
+	int x2[] = {1, 2};
 
 	// write test to verify that it returns a NULL pointer when the x pointer is NULL
 	int *p_work = linear(NULL, 1, 5, 10);
 	assert(p_work == NULL, "input x pointer is NULL but did not get a null pointer response") // don't try to print out an unknown pointer
 		? passcount++ : failcount++;
+	free(p_work);
 
 	// insert a test for count > 0 before doing this next test!
-	// verify that it returns a NULL pointer when the count is <0 =
-	p_work = linear(x1, -1, 5, 10);
+	// verify that it returns a NULL pointer when the count is <= 0
+	p_work = linear(x2, -1, 5, 10);
 	assert(p_work == NULL, "count is <= 0 but did not get a null pointer response")
 		? passcount++ : failcount++;
+	free(p_work);
 
 	// verify a null pointer isn't returned when x is valid
-	p_work = linear(x1, 1, 5, 10);
-	assert(p_work != NULL, "input x poitnter is valid but got a null pointer response")
+	p_work = linear(x2, sizeof(x2) / sizeof(int), 5, 10);
+	assert(p_work != NULL, "input x pointer is valid but got a null pointer response")
 		? passcount++ : failcount++;
+	free(p_work);
+
+	// verify a null pointer isn't returned when count is valid
+	p_work = linear(x2, sizeof(x2) / sizeof(int), 5, 10);
+	assert(p_work != NULL, "count is valid but got a null pointer response")
+		? passcount++ : failcount++;
+	free(p_work);
+
+	// x array has one integer, m = 2, b = 0
+	p_work = linear(x1, sizeof(x1) / sizeof(int), 2, 0);
+	int expected1[] = {2};
+	if (p_work < (int *)1000)	// avoid bogus pointer value crashes
+	{
+		// force the assert to fail to report a bad pointer
+		assert( 0,
+			"Bad pointer!")
+			? passcount++ : failcount++ ;
+	}
+	else
+	assert(memcmp(p_work, expected1, sizeof(expected1)) == 0, "expected array {2} but pointer was {%d}", *p_work)
+		? passcount++ : failcount++;
+	free(p_work);
+
+	// x array has two integers, m = 4, b = 2
+	p_work = linear(x2, sizeof(x2) / sizeof(int), 4, 2);
+	int expected2[] = {6, 10};
+	if (p_work < (int *)1000)	// avoid bogus pointer value crashes
+	{
+		// force the assert to fail to report a bad pointer
+		assert( 0,
+			"Bad pointer!")
+			? passcount++ : failcount++ ;
+	}
+	else
+	assert(memcmp(p_work, expected2, sizeof(expected2)) == 0, "expected array {6, 10} but pointer was {%d, %d}", p_work[0], p_work[1])
+		? passcount++ : failcount++;
+	// as per directions, not doing: free(p_work);
+
+
 
 	printf("Passed: %d, Failed: %d\n", passcount, failcount);
+
 	return failcount;	// return number of tests that failed
 }
 
